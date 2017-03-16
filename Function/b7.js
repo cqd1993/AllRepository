5928f539670d088885100808b0b62f83a1ffb237
cqd1993 <email>
2017-03-16 17:15:36 +0700

    thay doi bai7

--- Function/b7.js
+++ Function/b7.js
@@ -2,33 +2,34 @@ var teacher = {
   firstName: "Kim",
   lastName: "Dung",
   age: 30,
-  say: function(){
-   return this.firstName + this.lastName + this.age
- }
+ 
+ 
 }
 var student = {
 	firstName: "Con",
 	lastName: "Con",
 	age: 18,
-	say : function(){return this.firstName + this.lastName + this.age
- }
+	 
+ 
 }
 var parent = {
 	firstName: "bobo",
 	lastName: "bobo",
 	age: 40,
-	say: function(){
-		return this.firstName + this.lastName + this.age
-	}
+	say:function(){}
+	
 }
-console.log(teacher.say());
+//console.log(teacher.say());
 var aboutMe = function(){
+						
+						document.write("ten tôi là:"+this.firstName + this.lastName + this.age +"<br>");
+					}
 // dạng anymustfunction;
 
-	 document.write("xin chào các bạn! tôi là:");
-	 teacher.say();
+	/* document.write("xin chào các bạn! tôi là:");
+	 teacher.say();*/
  
-}
+
 /*function aboutMe()
 {
 	document.write("xin chào các bạn! tôi là:"+ teacher.say());
@@ -36,7 +37,15 @@ var aboutMe = function(){
 
 var b = aboutMe();*/
 //teacher.say();
-teacher.say = aboutMe();
-aboutMe = teacher.say();
+//parent.say();
+//parent.middername = "quang";
+teacher.say = aboutMe;
+parent.say = aboutMe;// tạo mới một hàm say;
+student.say = aboutMe;
+teacher.say();
+parent.say();
+student.say();
+//teacher.say();
+//aboutMe = teacher.say();
 //document.write(aboutMe);
-//teacher.say();   // In ra màn hình dòng chữ "Xin chào, tôi là Kim Dung. Năm nay tôi 30 tuổi."
+//teacher.say();   // In ra màn hình

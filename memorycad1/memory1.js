8772c4630ab6d378d5f28ada37f5f1f6c0089010
cqd1993 <email>
2017-04-07 16:25:49 +0700

    memorycard chi lat card chua so sanh duoc card

---
+++ memorycad1/memory1.js
@@ -0,0 +1,9 @@
+
+var arrt = ["m_n1","m_n2"];
+var arrs = ["back", "back1"];
+for(var i = 0; i<= arrt.length; i++)
+{
+	var c = $("font").html("asdasdasdas");
+	 arrt[i] = c;
+	$("font1").css("color","red") = arrt[i];
+}

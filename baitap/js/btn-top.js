703647408bcee1e47f79d1a7ff7c77f006583d38
cqd1993 <email>
2017-05-03 15:02:56 +0700

    do_an

---
+++ baitap/js/btn-top.js
@@ -0,0 +1,57 @@
+ $(document).ready(function(){
+     $(window).scroll(function () {
+            if ($(this).scrollTop() > 50) {
+                $('#back-to-top').fadeIn();
+            } else {
+                $('#back-to-top').fadeOut();
+            }
+        });
+        // scroll body to 0px on click
+        $('#back-to-top').click(function () {
+            $('#back-to-top').tooltip('hide');
+            $('body,html').animate({
+                scrollTop: 0
+            }, 800);
+            return false;
+        });
+        
+        $('#back-to-top').tooltip('show');
+});
+
+
+ // sliceshow
+
+$('#myCarousel').carousel({ 
+    interval:   4000    
+});
+
+
+///sliceshow totalchungcumini
+
+ $(document).ready(function(){
+     $(window).scroll(function () {
+            if ($(this).scrollTop() > 50) {
+                $('#back-to-top').fadeIn();
+            } else {
+                $('#back-to-top').fadeOut();
+            }
+        });
+        // scroll body to 0px on click
+        $('#back-to-top').click(function () {
+            $('#back-to-top').tooltip('hide');
+            $('body,html').animate({
+                scrollTop: 0
+            }, 800);
+            return false;
+        });
+        
+        $('#back-to-top').tooltip('show');
+});
+
+
+ // sliceshow
+
+$('#myCarousel-1').carousel({ 
+    interval:   4000    
+});
+
